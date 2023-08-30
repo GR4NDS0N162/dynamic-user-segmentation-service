d3661a9aa0612222edfb3359a3510e7afa19db4f
Kirill Vnukov <kirillgrandson@gmail.com>
2023-08-30 21:15:06 +0300

    Add repository methods to add/remove a user to/from a segment

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -48,3 +48,61 @@ func (r *Repository) DeleteSegment(slug string) (deleted bool, err error) {
 	result = r.db.Delete(&segment)
 	return result.RowsAffected != 0, result.Error
 }
+
+func (r *Repository) GetSegments(slugs []string) ([]model.Segment, error) {
+	var segments []model.Segment
+	result := r.db.Where("slug IN ?", slugs).Find(&segments)
+	return segments, result.Error
+}
+
+func (r *Repository) AddUserToSegments(userId int, segments []model.Segment) error {
+	user := model.Segment{ID: userId}
+	result := r.db.FirstOrCreate(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	for _, segment := range segments {
+		action := model.Action{UserID: userId, SegmentID: segment.ID}
+		result = r.db.Last(&action, action)
+		if !errors.Is(result.Error, gorm.ErrRecordNotFound) && action.Type == 0 { // Пользователь уже в сегменте
+			continue
+		} else if result.Error != nil {
+			return result.Error
+		}
+
+		action = model.Action{UserID: userId, SegmentID: segment.ID, Type: 0}
+		result = r.db.Create(&action)
+		if result.Error != nil {
+			return result.Error
+		}
+	}
+
+	return nil
+}
+
+func (r *Repository) RemoveUserFromSegments(userId int, segments []model.Segment) error {
+	user := model.Segment{ID: userId}
+	result := r.db.FirstOrCreate(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	for _, segment := range segments {
+		action := model.Action{UserID: userId, SegmentID: segment.ID}
+		result = r.db.Last(&action, action)
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) || action.Type == 1 { // Пользователь не в сегменте
+			continue
+		} else if result.Error != nil {
+			return result.Error
+		}
+
+		action = model.Action{UserID: userId, SegmentID: segment.ID, Type: 1}
+		result = r.db.Create(&action)
+		if result.Error != nil {
+			return result.Error
+		}
+	}
+
+	return nil
+}
