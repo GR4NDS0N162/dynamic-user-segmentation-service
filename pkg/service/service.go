75c6f0c8e24a099241abb7b9e228040b6216f43a
Kirill Vnukov <kirillgrandson@gmail.com>
2023-08-30 18:12:54 +0300

    Add a service method that adds the segment

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,10 +1,18 @@
 package service
 
-import "github.com/GR4NDS0N162/dynamic-user-segmentation-service/pkg/repository"
+import (
+	"github.com/GR4NDS0N162/dynamic-user-segmentation-service/model"
+	"github.com/GR4NDS0N162/dynamic-user-segmentation-service/pkg/repository"
+)
 
 type Service struct {
+	repository *repository.Repository
 }
 
 func NewService(repo *repository.Repository) *Service {
-	return &Service{}
+	return &Service{repository: repo}
+}
+
+func (s *Service) CreateSegment(slug string) (model.Segment, error) {
+	return s.repository.CreateSegment(slug)
 }
