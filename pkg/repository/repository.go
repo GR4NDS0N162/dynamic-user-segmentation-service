8026cbace670311c95811977ad3eb9c5fc0ad09d
Kirill Vnukov <kirillgrandson@gmail.com>
2023-08-30 18:12:33 +0300

    Add a repository method that adds the segment

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"github.com/GR4NDS0N162/dynamic-user-segmentation-service/model"
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +13,24 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) CreateSegment(slug string) (model.Segment, error) {
+	segment := model.Segment{Slug: slug}
+	result := r.db.Unscoped().FirstOrCreate(&segment, segment)
+
+	if result.Error != nil {
+		return segment, result.Error
+	}
+
+	if result.RowsAffected == 0 && segment.IsDel == 0 {
+		return segment, fmt.Errorf("segment %s already exists", slug)
+	}
+
+	if segment.IsDel == 1 {
+		segment.IsDel = 0
+		r.db.Save(&segment)
+	}
+
+	return segment, nil
 }
